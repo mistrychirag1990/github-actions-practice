6283a740811b11492a4f9b28f5f26e864a1c55a4
Inventam <inventam@kind-brahmagupta.122-170-11-110.plesk.page>
2026-04-20 14:29:22 +0530

    add chikky

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-  res.end("Hello from CI/CD by chirag 🚀");
+  res.end("Hello from CI/CD by chikky 🚀");
 }).listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
